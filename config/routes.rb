ccd2fc30b3a63ad78923224d7919f1a63baa6d89
Alicia Prather <Alprather@gmail.com>
2016-02-26 09:51:18 +0400

    user profile viewable

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   resources :topics do
-      resources :bookmarks
-    end
+    resources :bookmarks
+  end
   devise_for :users
+  resources :users, only: [:show]
   get 'welcome/index'
 
   get 'welcome/about'
